35083484a33bc955960c2beba9117d9a82af735e
Tanja Chigik <tanjachigik@gmail.com>
2025-03-22 19:15:32 +0200

    mongodb-4

--- src/server.js
+++ src/server.js
@@ -3,12 +3,10 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
-// Читаємо змінну оточення PORT
-const PORT = Number(getEnvVar('PORT'));
 
 export const setupServer = () => {
   const app = express();
-
+  const PORT = Number(getEnvVar('PORT'));
   app.use(express.json());
   app.use(
     cors({
@@ -27,19 +25,6 @@ export const setupServer = () => {
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello World!',
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
     });
   });
 
@@ -68,6 +53,20 @@ export const setupServer = () => {
       message: `Successfully found contact with id ${contactId}!`,
     });
   });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
