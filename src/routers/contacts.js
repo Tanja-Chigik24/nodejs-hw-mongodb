4e54ddb6c45d13f2256d2f8fae7a70935fbe2fa4
Tanja Chigik <tanjachigik@gmail.com>
2025-04-18 19:12:59 +0300

    hw5-auth+

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,30 +13,42 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
+import { checkRoles } from '../middlewares/checkRoles.js';
+import { ROLES } from '../constants/index.js';
 
-const router = Router();
+const contactsRouter = Router();
 
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get(
-  '/contacts/:contactId',
+contactsRouter.use(authenticate);
+contactsRouter.get(
+  '/',
+  checkRoles(ROLES.USER),
+  ctrlWrapper(getContactsController),
+);
+contactsRouter.get(
+  '/:contactId',
+  checkRoles(ROLES.USER),
   isValidId,
   ctrlWrapper(getContactByIdController),
 );
-router.post(
-  '/contacts',
+contactsRouter.post(
+  '/',
+  checkRoles(ROLES.USER),
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
-router.delete(
-  '/contacts/:contactId',
+contactsRouter.delete(
+  '/:contactId',
+  checkRoles(ROLES.USER),
   isValidId,
   ctrlWrapper(deleteContactController),
 );
-router.patch(
-  '/contacts/:contactId',
+contactsRouter.patch(
+  '/:contactId',
+  checkRoles(ROLES.USER),
   isValidId,
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
-export default router;
+export default contactsRouter;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,10 @@
 import { ContactsCollection } from '../db/models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import { SORT_ORDER } from '../constants/index.js';
+import { registerUser } from './auth.js';
 
 export const getAllContacts = async ({
+  userId,
   page,
   perPage,
   sortOrder = SORT_ORDER.ASC,
@@ -12,7 +14,7 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
+  const contactsQuery = ContactsCollection.find({ userId });
 
   if (filter.contactType) {
     contactsQuery.where('contactType').equals(filter.contactType);
@@ -22,7 +24,7 @@ export const getAllContacts = async ({
   }
 
   const [contactsCount, contacts] = await Promise.all([
-    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+    ContactsCollection.find({ userId }).merge(contactsQuery).countDocuments(),
     contactsQuery
       .skip(skip)
       .limit(limit)
@@ -37,8 +39,11 @@ export const getAllContacts = async ({
   };
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
+export const getContactById = async ({ _id, userId }) => {
+  const contact = await ContactsCollection.findOne({
+    _id: _id,
+    userId: userId,
+  });
   return contact;
 };
 
@@ -47,17 +52,17 @@ export const createContact = async (payload) => {
   return contact;
 };
 
-export const deleteContact = async (contactId) => {
+export const deleteContact = async ({ _id, userId }) => {
   const contact = await ContactsCollection.findOneAndDelete({
-    _id: contactId,
+    _id: _id,
+    userId: userId,
   });
-
   return contact;
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
+export const updateContact = async ({ _id, userId }, payload, options = {}) => {
   const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
+    { _id: _id, userId: userId },
     payload,
     {
       new: true,
@@ -65,7 +70,7 @@ export const updateContact = async (contactId, payload, options = {}) => {
       ...options,
     },
   );
-
+  console.log(rawResult);
   if (!rawResult || !rawResult.value) return null;
 
   return {
